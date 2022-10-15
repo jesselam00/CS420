817b5fb5c43efab4714d9ffd76a4720553a3b664
Dylan <dylcal138@gmail.com>
2022-10-15 11:41:58 -0500

    Singleton Dashboard is STUPID

--- CS420/src/main/java/com/cs420/cs420/Dashboard.java
+++ CS420/src/main/java/com/cs420/cs420/Dashboard.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dash-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
