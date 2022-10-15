eeb9e53cd0fbabda412e938425ef451e2dde6169
Jesse Lam <84208649+jesselam00@users.noreply.github.com>
2022-10-15 12:24:34 -0500

    Change fxml file

--- CS420/src/main/java/com/cs420/cs420/Dashboard.java
+++ CS420/src/main/java/com/cs420/cs420/Dashboard.java
@@ -11,8 +11,8 @@ public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dash-scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setTitle("Farm Dashboard");
         stage.setScene(scene);
         stage.show();
     }
