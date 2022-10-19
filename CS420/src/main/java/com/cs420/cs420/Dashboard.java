8d64477f4453d2784459ce474bd8cbb7acfd34d3
Dylan <dylan@DESKTOP-I1K1LHG.localdomain>
2022-10-19 13:51:29 -0500

    singleton dashboard IS STUPID 2

--- CS420/src/main/java/com/cs420/cs420/Dashboard.java
+++ CS420/src/main/java/com/cs420/cs420/Dashboard.java
@@ -1,25 +1,23 @@
 package com.cs420.cs420;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Dashboard {
-    private static Dashboard dashboard;
-    public Scene scene;
+import java.io.IOException;
 
-    private Dashboard(){
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dash-scene.fxml"));
-            scene = new Scene(fxmlLoader.load(), 1024, 768);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+public class Dashboard extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dash-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 800);
+        stage.setTitle("Project");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public static Dashboard getInstance(){
-        if (dashboard == null){
-            dashboard = new Dashboard();
-        }
-        return dashboard;
+    public static void main(String[] args) {
+        launch();
     }
 }
--- CS420/src/main/java/com/cs420/cs420/DashboardController.java
+++ CS420/src/main/java/com/cs420/cs420/DashboardController.java
@@ -2,13 +2,18 @@ package com.cs420.cs420;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
 public class DashboardController {
     @FXML
-    private Label welcomeText;
+    private TreeView FarmObjects;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize(){
+        FarmObjects.setRoot(new TreeItem<>("Test Root"));
+
     }
+
 }
